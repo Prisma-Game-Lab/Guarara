f5250897fc0e82002e20d034a25262f4d2ccbe47
Luiza-Arantes <luiza.acc10@gmail.com>
2023-05-15 19:52:01 -0300

    MenuManager e AudioConfig

---
+++ Assets/Scripts/Sistemas/MenuManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private string cenaInicial;
+    [SerializeField] private GameObject painelMenuPrincipal;
+    [SerializeField] private GameObject painelAjustes;
+
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(cenaInicial);
+    }
+
+    public void AbrirAjustes()
+    {
+        painelMenuPrincipal.SetActive(false);
+        painelAjustes.SetActive(true);
+    }
+    
+    public void FecharAJustes()
+    {   
+        painelAjustes.SetActive(false);
+        painelMenuPrincipal.SetActive(true);
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+
+
+}
