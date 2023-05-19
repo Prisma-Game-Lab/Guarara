7854b15b36233bf000cd2ed1578c0aedfcc216cb
Luiza-Arantes <luiza.acc10@gmail.com>
2023-05-19 19:54:01 -0300

    Conserta AudioConfig e AudioManager

--- Assets/Scripts/Sistemas/AudioConfig.cs
+++ Assets/Scripts/Sistemas/AudioConfig.cs
@@ -7,9 +7,12 @@ public class AudioConfig : MonoBehaviour
 {
     private static readonly string firstPlay = "FirstPlay";
     private static readonly string volumePref = "VolumePref";
+    private static readonly string sfxPref = "SfxPref";
     private int firstPlayInt;
     public Slider volumeSlider;
+    public Slider sfxSlider;
     private float volumeFloat;
+    private float sfxFloat;
     private AudioManager _am;
     // Start is called before the first frame update
     void Start()
@@ -23,18 +26,30 @@ public class AudioConfig : MonoBehaviour
             volumeFloat = 0.5f;
             volumeSlider.value = volumeFloat;
             PlayerPrefs.SetFloat(volumePref, volumeFloat);
+
+            sfxFloat = 0.5f;
+            sfxSlider.value = volumeFloat;
+            PlayerPrefs.SetFloat(sfxPref, sfxFloat);
+
             PlayerPrefs.SetInt(firstPlay, -1);
+
         }
         else
         {
             volumeFloat = PlayerPrefs.GetFloat(volumePref);
             volumeSlider.value = volumeFloat;
+
+            sfxFloat = PlayerPrefs.GetFloat(sfxPref);
+            sfxSlider.value = sfxFloat;
         }
     }
+
 
     public void SaveSoundSettings()
     {
         PlayerPrefs.SetFloat(volumePref, volumeSlider.value);
+        PlayerPrefs.SetFloat(sfxPref, sfxSlider.value);
+
     }
 
     private void OnApplicationFocus(bool inFocus)
@@ -48,6 +63,14 @@ public class AudioConfig : MonoBehaviour
     public void UpdateSound()
     {
         foreach (Sound s in _am.sounds)
+        {
+            s.source.volume = volumeSlider.value;
+        }
+        SaveSoundSettings();
+    }
+    public void UpdateSfx()
+    {
+        foreach (Sound s in _am.sfx)
         {
             s.source.volume = volumeSlider.value;
         }
