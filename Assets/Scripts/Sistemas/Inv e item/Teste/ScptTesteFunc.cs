d6810c86c13dfcf71aa8b7949e01bc94fe2ac8b7
R0bbyt3 <130287531+R0bbyt3@users.noreply.github.com>
2023-05-19 15:07:35 -0300

    Test Dialog-Inv

--- Assets/Scripts/Sistemas/Inv e item/Teste/ScptTesteFunc.cs
+++ Assets/Scripts/Sistemas/Inv e item/Teste/ScptTesteFunc.cs
@@ -1,11 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScptTesteFunc : MonoBehaviour
 {
-    public void FuncaoTeste()
+    [SerializeField]
+    GameObject TextoPrefab; 
+    [SerializeField]
+    GameObject CanvasParent;
+
+    public void FuncaoTestePorta1()
     {
-        print("odeio todos os viados, inclusive eu");
+        GameObject texto = Instantiate(TextoPrefab);
+        texto.transform.SetParent(CanvasParent.transform, false);
+        RectTransform rectTransform = texto.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(0, -350);
+        texto.GetComponent<TextMeshProUGUI>().text = "A porta 1 foi aberta e essa função ligada a ela foi ativa!";
+    }
+
+    public void FuncaoTestePorta2()
+    {
+        GameObject texto = Instantiate(TextoPrefab);
+        texto.transform.SetParent(CanvasParent.transform, false);
+        RectTransform rectTransform = texto.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(0, 0);
+        texto.GetComponent<TextMeshProUGUI>().text = "A porta 2 foi aberta e essa função ligada a ela foi ativa!";
+    }
+
+    public void FuncaoTestePorta3()
+    {
+        GameObject texto = Instantiate(TextoPrefab);
+        texto   .transform.SetParent(CanvasParent.transform, false);
+        RectTransform rectTransform = texto.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(0, 320);
+        texto.GetComponent<TextMeshProUGUI>().text = "A porta 3 foi aberta e essa função ligada a ela foi ativa!";
     }
 }
