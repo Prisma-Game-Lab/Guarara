34e97f8a23a8190d78465ab1ed9726a775d8f237
Apenasumcara <vscamerano@gmail.com>
2023-07-27 15:15:02 -0300

    Dialogo automatico para o tutorial

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Conversation conversation;
+    [Tooltip("Marcar quando o diálogo começar automaticamente assim que a cena carrega.")]
+    public bool isAutomatic;
 
     public void OnStartConversation ()
     {
         FindObjectOfType<DialogueManager>().StartConversation(conversation);
         
+    }
+
+    void Start()
+    {
+        if(isAutomatic)
+        {
+            OnStartConversation();
+        }
     }
 }
