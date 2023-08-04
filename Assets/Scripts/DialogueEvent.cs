4c71130df890060e598d4599fd2e64f1455004e5
jmal93 <marcello.jao1611@gmail.com>
2023-08-04 15:32:02 -0300

    conserto bug passar o diálogo com "E"

--- Assets/Scripts/DialogueEvent.cs
+++ Assets/Scripts/DialogueEvent.cs
@@ -5,27 +5,27 @@ using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 
-public class DialogueEvent : MonoBehaviour
+public class DialogueEvent : MonoBehaviour, IInteractable
 {
-   public UnityEvent OnEKeyPressed;
-   public InputActionAsset action;
-   private InputAction interactionAction;
+    public UnityEvent OnEKeyPressed;
+    public InputActionAsset action;
+    private InputAction interactionAction;
 
-   public void StartConversation ()
+    public void StartConversation()
     {
-        if(GetComponent<IsNearEvent>().wasCollided == true && GetComponent<IsNearEvent>().characterTag=="NPC")
+        if (GetComponent<IsNearEvent>().wasCollided == true && GetComponent<IsNearEvent>().characterTag == "NPC")
         {
             OnEKeyPressed?.Invoke();
         }
     }
-   private void OnEPressed(InputAction.CallbackContext context)
-   {
+    public void Interact()
+    {
         StartConversation();
-   }
+    }
 
-   void Awake()
-   {
-        action.FindActionMap("Player").FindAction("Interact").performed += OnEPressed;
-        
-   }
+    //    void Awake()
+    //    {
+    //         action.FindActionMap("Player").FindAction("Interact").performed += OnEPressed;
+
+    //    }
 }
