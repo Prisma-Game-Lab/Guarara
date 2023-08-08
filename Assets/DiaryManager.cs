ac58ea4ebcc9aba2a9eca5e2f7a2a20f1fb1f0ed
MadudaJellyfish <105326941+MadudaJellyfish@users.noreply.github.com>
2023-08-08 20:44:37 -0300

    alterações no script do diário (funciona pvfr)

--- Assets/DiaryManager.cs
+++ Assets/DiaryManager.cs
@@ -7,32 +7,33 @@ using UnityEngine.InputSystem;
 public class DiaryManager : MonoBehaviour
 {
     public UnityEvent QPressed;
-    // private InputAction interactionAction;
+    private InputAction interactionAction;
     public InputActionAsset action2;
     [SerializeField] public GameObject[] pagesRight;
     [SerializeField] public GameObject[] pagesLeft;
-    private int currentPageRight;
-    private int currentPageLeft;
+    private int currentPageRight = 0;
+    private int currentPageLeft = 0;
 
     public GameObject setButton1;
     public GameObject setButton2;
     private bool wasClicked = false;
-
-    public void ActiveDiary() //Essa funcao vai ficar no evento OnClick do inspector do botao que e o diario
+    
+    public void ActiveDiary () //Essa funcao vai ficar no evento OnClick do inspector do botao que e o diario
     {
-        if (wasClicked == false)              //O diário é ativado e desativado no mesmo botão
+        if (wasClicked==false)              //O diário é ativado e desativado no mesmo botão
         {
-            currentPageRight = 0;
-            foreach (var pageRight in pagesRight)
+		    foreach (var pageRight in pagesRight ) 
             {
-                pageRight.SetActive(false);
-            }
+			    pageRight.SetActive(false);
+		    }
 
             pagesRight[currentPageRight].SetActive(true);
+            pagesLeft[currentPageLeft].SetActive(true);
             setButton1.SetActive(true);
+            setButton2.SetActive(true);
             wasClicked = true;
         }
-        else if (wasClicked == true)
+        else if (wasClicked==true)
         {
             pagesRight[currentPageRight].SetActive(false);
             pagesLeft[currentPageLeft].SetActive(false);
@@ -40,83 +41,65 @@ public class DiaryManager : MonoBehaviour
             setButton2.SetActive(false);
             wasClicked = false;
         }
-
-
+        
+        
     }
-    // public void OnQPressed(InputAction.CallbackContext context)
-    // {
-    //     QPressed?.Invoke();
-    // }
-
-    // void Awake ()
-    // {
-    //     action2.FindActionMap("Player").FindAction("Interact").performed += OnQPressed;
-    // }
-    private void SetPageRight(int pageRight)
+    public void OnQPressed (InputAction.CallbackContext context)
     {
-        pagesRight[currentPageRight].SetActive(false);
-        currentPageRight = pageRight;
-        pagesRight[currentPageRight].SetActive(true);
+        QPressed?.Invoke();
     }
 
-
-    private void SetPageLeft(int pageLeft)
+    void Awake ()
     {
-        pagesLeft[currentPageLeft].SetActive(false);
-        currentPageLeft = pageLeft;
-        pagesLeft[currentPageLeft].SetActive(true);
+        action2.FindActionMap("Player").FindAction("Interact").performed += OnQPressed;
     }
+	private void SetPageRight(int pageRight) 
+	{
+		pagesRight[currentPageRight].SetActive(false);
+		currentPageRight = pageRight;
+		pagesRight[currentPageRight].SetActive(true);
+	}
 
-    public void ActiveLeftPages()
+    
+	private void SetPageLeft(int pageLeft) 
+	{
+		pagesLeft[currentPageLeft].SetActive(false);
+		currentPageLeft = pageLeft;
+		pagesLeft[currentPageLeft].SetActive(true);
+	}
+
+    /* public void ActiveLeftPages ()
     {
-        currentPageLeft = 0;
-        foreach (var pageLeft in pagesLeft)
+         currentPageLeft = 0;
+		foreach (var pageLeft in pagesLeft ) 
         {
             pageLeft.SetActive(false);
-        }
+		}
 
         pagesLeft[currentPageLeft].SetActive(true);
         setButton2.SetActive(true);
-    }
-    public void GotoNextPage()
+    } */
+	public void GotoNextPage()
     {   //Preciso de um "for" que limite o acréscimo de páginas
-
-        if (currentPageRight > pagesRight.Length)
+        currentPageRight++;
+        currentPageLeft++;
+        if(currentPageRight<=pagesRight.Length && currentPageLeft<=pagesLeft.Length)
         {
-            currentPageRight = 0;
+            SetPageRight(int currentPageRight);
+            SetPageLeft(int currentPageLeft);
         }
-        else
-        {
-            currentPageRight += 1;
-        }
-        SetPageRight(currentPageRight);
-        SetPageLeft(currentPageRight);
-
-        // for(int i = currentPageRight; i < pagesRight.Length;i++)
-        // {
-
-        // }
-
+		
     }
-    public void GotoPreviousPage()
+	public void GotoPreviousPage()
     {
-        // for(int i = currentPageLeft; i>0 ;i--)
-        // {
-        //     SetPageRight(i);
-        //     SetPageLeft(i);
-        // }
-        //Preciso de um "for" que limite o acréscimo de páginas
-
-        if (currentPageLeft < pagesLeft.Length)
+        currentPageRight--;
+        currentPageLeft--;
+        if(currentPageRight>=0 && currentPageLeft>=0)
         {
-            currentPageLeft = 0;
+            SetPageRight(int currentPageRight);
+            SetPageLeft(int currentPageLeft);
         }
-        else
-        {
-            currentPageLeft -= 1;
-        }
-        SetPageRight(currentPageLeft);
-        SetPageLeft(currentPageLeft);
-
+		
+		
     }
 }
